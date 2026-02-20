152b257bd77bb0189d1f8a878ef83bdbb52830a2
SaruM4N3 <zakobajo@gmail.com>
2026-02-20 21:53:55 +0100

    fix mouse

--- engine/src/core/inputs/mouse.c
+++ engine/src/core/inputs/mouse.c
@@ -6,7 +6,7 @@
 /*   By: zsonie <zsonie@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 02:53:40 by dderny            #+#    #+#             */
-/*   Updated: 2026/02/20 21:18:18 by zsonie           ###   ########lyon.fr   */
+/*   Updated: 2026/02/20 21:53:16 by zsonie           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,15 @@ int	inputs_mousedown(int key, t_engine *engine)
 	return (0);
 }
 
+static float	sanitize_angle(float f)
+{
+	if (f > 360)
+		f -= 360;
+	else if (f < 0)
+		f += 360;
+	return (f);
+}
+
 int	inputs_mousemove(int x, int y, t_engine *engine)
 {
 	int	center_x;
@@ -28,7 +37,9 @@ int	inputs_mousemove(int x, int y, t_engine *engine)
 	center_y = engine->window.height / 2;
 	if (x == center_x && y == center_y)
 		return (0);
-	engine->camera.rot.z += (x - center_x) * engine->camera.rot_speed;
+	engine->camera.rot.z += ((x - center_x) * engine->camera.rot_speed
+			* engine->frametime);
+	sanitize_angle(engine->camera.rot.z);
 	mlx_mouse_move(engine->window.mlx, engine->window.mlx_win, center_x,
 		center_y);
 	return (0);

8130fb33928583b081c14d2e4270a9b219fe0ad0
David Derny <dderny@student.42lyon.fr>
2026-02-20 21:54:20 +0100

    fix: norm

--- engine/src/main.c
+++ engine/src/main.c
@@ -6,7 +6,7 @@
 /*   By: zsonie <zsonie@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 16:14:42 by dderny            #+#    #+#             */
-/*   Updated: 2026/02/20 21:31:48 by dderny           ###   ########.fr       */
+/*   Updated: 2026/02/20 21:41:43 by dderny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,7 +48,7 @@ int	engine_initialize(t_engine *engine, int argc, char *argv[])
 	if (load_map(engine->window.mlx, &map))
 		return (1);
 	engine->map = &map;
-	engine->camera = (t_camera){.speed = 2.0f, .rot_speed = 500.0f, .fov = 75,
+	engine->camera = (t_camera){.speed = 2.0f, .rot_speed = 5.0f, .fov = 75,
 		.pos = {map.spawn.x - SPAWN_OFFSET, map.spawn.y - SPAWN_OFFSET, 0}};
 	if (game_initialize(engine))
 		return (1);
--- game/src/main.c
+++ game/src/main.c
@@ -6,30 +6,30 @@
 /*   By: zsonie <zsonie@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/14 22:51:55 by dderny            #+#    #+#             */
-/*   Updated: 2026/02/20 08:48:06 by zsonie           ###   ########lyon.fr   */
+/*   Updated: 2026/02/20 21:53:43 by dderny           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "core/game.h"
 
-const int g_win_width = 1920;
-const int g_win_height = 1080;
+const int	g_win_width = 1920;
+const int	g_win_height = 1080;
 
-const char *g_win_title = "Cub3d";
+const char	*g_win_title = "Cub3d";
 
-int game_initialize(t_engine *engine)
+int	game_initialize(t_engine *engine)
 {
 	mlx_mouse_hide(engine->window.mlx, engine->window.mlx_win);
 	return (0);
 }
 
-int game_update(t_engine *engine)
+int	game_update(t_engine *engine)
 {
 	(void)engine;
 	return (0);
 }
 
-int game_tick(t_engine *engine)
+int	game_tick(t_engine *engine)
 {
 	(void)engine;
 	return (0);
